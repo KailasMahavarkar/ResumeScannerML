6524e0e23cb25c7a6e3d4b361509a65bb7a29ee5
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2021-12-07 22:38:41 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import "../src/scss/main.css";
 import { MODE, SERVER } from "./helper";
+import "../src/scss/main.css";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./components/homepage";
@@ -9,16 +9,16 @@ import Resumepage from "./components/resumepage";
 import Navbar from "./components/navbar";
 import { useEffect } from "react";
 
-function App() {
 
-    useEffect(() => {
-        console.log("mode -->", MODE)
-        console.log("server -->", SERVER)
-    }, [])
+function App() {
+	useEffect(() => {
+		console.log("mode -->", MODE);
+		console.log("server -->", SERVER);
+	}, []);
 
 	return (
 		<Router>
-            <Route exact path="*" component={Navbar} />
+			<Route exact path="*" component={Navbar} />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route exact path="/scan" component={Scanpage} />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-
 
 ReactDOM.render(
 	<React.StrictMode>
